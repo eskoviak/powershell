77d27c4e6a1d496957ee04fb31a5d296e014b68e
Ed Skoviak <eskoviak@eskoviak.net>
2018-02-26 13:30:38 -0600

    General run of sql 2017 w/o preservation of table data

--- Invoke-SQLServerDocker.ps1
+++ Invoke-SQLServerDocker.ps1
@@ -4,4 +4,5 @@ $dbLocalFiles = "C:\Users\eskov\AppData\Local\Microsoft\Microsoft SQL Server Loc
 $attachDB = @(@{'dbName'=$dbName}, @{'dbfiles'=@($(Join-Path -Path $dbLocalFiles -ChildPath (($dbName)+".mdf")),
    $(Join-Path -Path $dbLocalFiles -ChildPath (($dbName)+".ldf")))})
 
-docker run -d -p 1433:1433 -e sa_password=$saPassword -e ACCEPT_EULA=Y -e attach_dbs=$attachDB microsoft/mssql-server-windows-developer
+#docker run -d -p 1433:1433 -e sa_password=$saPassword -e ACCEPT_EULA=Y -e attach_dbs=$attachDB microsoft/mssql-server-windows-developer
+docker run -d -p 1433:1433 -e sa_password=$saPassword -e ACCEPT_EULA=Y  microsoft/mssql-server-windows-developer
