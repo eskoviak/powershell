071e6b4fb35bf2ac5f5fef9e5b6a92bb4a78d043
Ed Skoviak <eskoviak@eskoviak.net>
2018-03-29 10:43:14 -0500

    erging and moving

--- Invoke-SQLServerDocker.ps1
+++ Invoke-SQLServerDocker.ps1
@@ -5,9 +5,9 @@ $saPassword = "Thagn0th%"
 #   $(Join-Path -Path $dbLocalFiles -ChildPath (($dbName)+".ldf")))})
 
 #HP-840
-$attachDB = "[{'dbName' : 'HealthFacts', 'dbFiles' : ['c:\\data\\HealthFacts.mdf', 'c:\\data\\HealthFacts.ldf']}]"
+#$attachDB = "[{'dbName' : 'HealthFacts', 'dbFiles' : ['c:\\data\\HealthFacts.mdf', 'c:\\data\\HealthFacts.ldf']}]"
 #EdGamer
 #$attachDB = "[{'dbName' : 'mytest', 'dbFiles' : ['c:\\data\\mytest.mdf', 'c:\\data\\mytest.ldf']}]"
 #docker run -d -p 1433:1433 -e sa_password=$saPassword -e ACCEPT_EULA=Y --mount type=bind,source="D:/SQLDatabases",target="c:/data" -e attach_dbs=$attachDB microsoft/mssql-server-windows-developer
-docker run -d -p 1433:1433 -e sa_password=$saPassword -e ACCEPT_EULA=Y  microsoft/mssql-server-windows-developer
-#docker run -d -p 1433:1433 -e sa_password=$saPassword -e ACCEPT_EULA=Y --mount type=bind,source="D:/SQLDatabases",target="c:/data" microsoft/mssql-server-windows-developer
+#docker run -d -p 1433:1433 -e sa_password=$saPassword -e ACCEPT_EULA=Y  microsoft/mssql-server-windows-developer
+docker run -d -p 1433:1433 -e sa_password=$saPassword -e ACCEPT_EULA=Y --mount type=bind,source="C:/Users/edskov/SQLDatabases",target="c:/data" microsoft/mssql-server-windows-developer
