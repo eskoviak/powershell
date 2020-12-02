9f25b4f6ac4b599a1bf4a9d81aa85e1735266ab4
Ed Skoviak <ed.skoviak@redwingshoes.com>
2020-12-02 16:18:48 -0600

    Added more AD Stuff

--- Get-ADUsers.ps1
+++ Get-ADUsers.ps1
@@ -1,25 +1,34 @@
 <##
     Base Types
 #>
+
+<## RWSCUsers #>
 $UserSource = @"
 public class User {
+    public string DistinguishedName { get; set; }
     public string GivenName { get; set; }
     public string Surname {get; set; }
+    public string Name { get { return this.DistinguishedName.Split(",")[0].Split('=')[1]; } }
+    public string Description { get; set; }
+    public string UserPrincipalName { get; set; }
+    public string SamAccountName { get; set; }
 }
 "@
 Add-Type -TypeDefinition $UserSource
 
 $UserProperties = ('GivenName', 'Surname', 'Name', 'Description', 'UserPrincipalName', 'SamAccountName')
-$SearchBase = 'OU=Houston,OU=RWSCUsers,DC=RWSC,DC=Net'
+$UserSearchBase = 'OU=Houston,OU=RWSCUsers,DC=RWSC,DC=Net'
 $Users = @()
 
-Get-ADUser -Filter * -SearchBase $SearchBase -Properties $UserProperties | ForEach-Object {
+Get-ADUser -Filter * -SearchBase $UserSearchBase -Properties $UserProperties | ForEach-Object {
     $User = New-Object User
+    $User.DistinguishedName = $_.DistinguishedName
     $User.GivenName = $_.GivenName
     $User.Surname = $_.Surname
+    $User.Description = $_.Description
+    $User.UserPrincipalName = $_.UserPrincipalName
+    $User.SamAccountName = $_.SamAccountName
     $Users += $User
 }
 
-foreach ($item in $Users) {
-    Write-Host('{0}:{1}' -f  $item.GivenName, $item.Surname) 
-}
+
---
+++ helper.ps1
@@ -0,0 +1,5 @@
+$string = "CN=Ed Skoviak,OU=Corporate,OU=RWSCUsers,DC=rwsc,DC=net"
+
+$OUs = @()
+
+
