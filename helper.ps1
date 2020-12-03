28e96ea1714627434ba7b83ed1f76e4798fc2bb4
Ed Skovia <eskoviak@gmail.com>
2020-12-02 19:44:16 -0600

    EOD Mac

--- helper.ps1
+++ helper.ps1
@@ -1,5 +1,16 @@
 $string = "CN=Ed Skoviak,OU=Corporate,OU=RWSCUsers,DC=rwsc,DC=net"
 
 $OUs = @()
+$frags = $string.split(',')
 
+foreach($frag in $frags){
+  $tmp = $frag.split('=')
+  if ($tmp[0] -eq 'OU') { $OUs += $tmp[1] }
+}
+
+foreach($OU in $OUs) {
+  Write-Output($OU)
+} 
+
+  
 
