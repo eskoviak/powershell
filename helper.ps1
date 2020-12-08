57fa730a19b21fb0ca85afd9ca84802a3c6743c4
Ed Skoviak <ed.skoviak@redwingshoes.com>
2020-12-08 16:08:18 -0600

    EOD

--- helper.ps1
+++ helper.ps1
@@ -12,20 +12,39 @@ foreach($frag in $frags){
 foreach($OU in $OUs) {
   Write-Output($OU)
 } 
+
+""
+
+$absoluteStatus = @"
+FOUND
+
+Strt:20201119162253
+
+RSLT:FOUND
+
+FLE:C:\BOOTSECT.BAK.hard2decrypt
+
+FN:20201119162253
+
+RT:0
+"@
+$eol = @("`r", "`r`n")
+$t = ($absoluteStatus.Split($eol, [System.StringSplitOptions]::RemoveEmptyEntries))
+
+write-output($t.Length)
+#$searchPhrase = 'drebell'
 #>
-
-$searchPhrase = 'drebell'
-
 $absoluteFile = '.\absoluteList.xlsx'
 
 if (Test-Path( $absoluteFile )) {
+  $absoluteData = Import-Excel -Path $absoluteFile -Worksheet Sheet1
 
-
-
-
-
-
+  #$absoluteData | Sort-Object -Property Model -Unique | Select-Object -Property Model
+  $tmp = ($absoluteData[1].FileDectectResultDiagnostics)
+  $Status = $tmp.Substring(0, $tmp.IndexOf(':')-6)
+  $StatusDt = $tmp.Substring($tmp.IndexOf(':')+1, 8)
+  $Status
+  $StatusDt
 }
-
   
 
