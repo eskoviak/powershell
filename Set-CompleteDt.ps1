169323c50ccfb7393a0962e7a184b6d7ee9c4bd3
Ed Skoviak <eskoviak@eskoviak.com>
2020-12-18 14:17:27 -0600

    EOD working on compl dt

--- Set-CompleteDt.ps1
+++ Set-CompleteDt.ps1
@@ -4,43 +4,30 @@
         record
 
 #>
-param(
+import-module  ImportExcel
 
-)
+#Set-Alias -Name gac -Value ./Get-ADComputers.ps1
+$ExcelFile = 'C:\Users\edskov\Documents\Excel\AD User List_with_device_status Master.xlsx'
+$ADComputers = Get-ADComputer -Filter * -Properties Whencreated
 
-$PackagePath = 'C:\Program Files\PackageManagement\NuGet\Packages\Microsoft.SharePoint.Client.dll.15.0.4420.1017'
+if(Test-Path $ExcelFile) {
+    $Details = Import-Excel -Path $ExcelFile -Worksheet Details
+    #Write-Host "No. Computers: $($ADComputers.Length)"
+    foreach ($row in $Details) {
+        Write-Host "Processing $($row.Name) ( $($row.'3-4 User Name') ) : " -NoNewLine
+        $found = $false
+        $ADComputers | ForEach-Object {
+                if ( $_.Name -like "*" + $row.'3-4 User Name' + "*" ) {
+                    if ($found) {
+                        " *" | Write-Host -NoNewLine
+                    }
+                    Write-Host "$($_.Name)" -NoNewLine
+                    $found = $true
+                }
+        }
+        Write-Host ""
+    }
 
-#Load SharePoint CSOM Assemblies
-Add-Type -Path (Join-Path -Path $PackagePath -ChildPath "Microsoft.SharePoint.Client.dll")
-Add-Type -Path (Join-Path -Path $PackagePath -ChildPath "Microsoft.SharePoint.Client.Runtime.dll")
-
-#Add-Type -Path "C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll"
-#Add-Type -Path "C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.Runtime.dll"
-    
-#Variables for Processing
-$SiteUrl = "https://redwingshoes.sharepoint.com/sites/TM_BT_Recovery"
-$FileRelativeURL ="https://redwingshoes.sharepoint.com/sites/TM_BT_Recovery/Shared Documents/General/Workstation Recovery/AD User List_with_device_status Master.xlsx"
- 
-#Get Credentials to connect
-$Cred = Get-Credential
-  
-Try {
-    #Set up the context
-    $Ctx = New-Object Microsoft.SharePoint.Client.ClientContext($SiteUrl)
-    exit
-    $Ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Cred.UserName,$Cred.Password)
-  
-    #powershell get file from sharepoint online
-    $File = $Ctx.web.GetFileByServerRelativeUrl($FileRelativeURL)
-    $Ctx.Load($File)
-    $Ctx.ExecuteQuery()
-  
-    Write-host "File Size:" ($File.Length/1KB)
+} else {
+    Write-Host "File $($ExcelFile) not found"
 }
-catch {
-    write-host "Error: $($_.Exception.Message)" -Foregroundcolor Red
-    Write-Host "Details: $($_.ErrorDetails.Message)" -ForegroundColor Blue
-}
-
-
-#Read more: https://www.sharepointdiary.com/2018/08/sharepoint-online-powershell-to-get-file.html#ixzz6gu4XbSnr
