24caef479980b1eaafb7fd093c21889d8f421386
Ed Skoviak <eskoviak@eskoviak.com>
2021-01-19 16:38:23 -0600

    eod

--- Get-AdUserHelper.ps1
+++ Get-AdUserHelper.ps1
@@ -1,64 +1,59 @@
 Import-Module ImportExcel
 
-$datafile = 'C:\Users\edskov\Downloads\SCCM Update.xlsx'
-$outputfile = 'C:\Users\edskov\Documents\Excel\RemediationList.xlsx'
+$datafile = 'C:\Users\edskov\Downloads\New List for SCCM Remediation.xlsx'
+$outputfile = 'C:\Users\edskov\Documents\Excel\RemediationList-1.xlsx'
+
 Add-Type -TypeDefinition @"
 using System;
 
 public class ExcelRow {
     public string DistinguishedName { get; set; }
     public string SamAccountName { get; set; }
+    public string OU { get; set; }
     public string UserPrincipalName { get; set; }
     public string CompletionDt { get; set; }
     public string Notes { get; set; }
-    public bool Skip { get; set; }
+    // public bool Skip { get; set; }
 
 }
 "@
 
-$emailBody = @"
-RWSC User--
-
-Over the course of the next few days, it will be necessary for one of our Help Desk Technicians to remote into your work device to perform software maintenance.
-
-This activity will not require any action on your part.  The technician will attempt to contact you prior to starting the activity via Teams.  If you plan on
-being away from your machine, please leave it connected to the internet, powered on, and unlocked.  Thank you in advance for your cooperation.  --IT Help Desk
-"@
-
 $ExcelRows = @()
-
 if(Test-Path $datafile) {
-    $data = Import-Excel -Path $datafile -WorksheetName 'Normalized Data'
+    $data = Import-Excel -Path $datafile -WorksheetName 'Sheet2'
     Write-Output ("Rows read: $($data.Length)")
     foreach ($row in $data) {
         $rowOut = New-Object ExcelRow
-        $skip = $false
         $notes = [String]::Empty
-        $CompletionDt = $row.CompletionDt
-        if ($CompletionDt -ne [String]::Empty) { continue }
-        $samAccountName = $row.Name.split('-')[0]
+        $samAccountName = $row.'Device name'.split('-')[0]
         $userData = (Get-ADUser -Filter {SamAccountName -like $samaccountname})
         $UserPrincipalName = $userData.UserPrincipalName
         if ($UserPrincipalName.Length -eq 0) {
             $Notes = 'Not an user account'
-            $skip = $true
         }
         $DistinguishedName = $userData.DistinguishedName
-        if ($DistinguishedName -like '*OU=STJ*') {
-            $Notes = 'OU=SJH'
-            $Skip = $true
+        if ($null -ne $DistinguishedName ) {
+            $DNArray = $DistinguishedName.Split(',')
+            $rowOut.OU = $DNArray[1]
+        } else {
+            $rowOut.OU = [String]::Empty  
         }
+        ##if ($DistinguishedName -like '*OU=STJ*') {
+        ##    $Notes = 'OU=SJH'
+        ##    $Skip = $true
+        ##}
+        
         $rowOut.DistinguishedName = $DistinguishedName
         $rowOut.SamAccountName  = $samAccountName
         $rowOut.UserPrincipalName = $UserPrincipalName
-        $rowOut.CompletionDt = $CompletionDt
+        $rowOut.CompletionDt = $null
         $rowOut.Notes = $notes
-        $rowOut.Skip = $skip
         $ExcelRows += $rowOut
 
-        #Write-Output $samAccountName, $UserPrincipalName, $DistinguishedName, $Notes, $CompletionDt,$skip
+        
+        #Write-Output $samAccountName, $UserPrincipalName, $DistinguishedName, $Notes, $CompletionDt,$DNArray[1]
     }
     Export-Excel -Path $outputfile -InputObject $ExcelRows -WorksheetName 'Client List'
 } else {
-    Write-Output ("File $($TestPath) not found")
+    Write-Output ("File $($datafile) not found")
 }
